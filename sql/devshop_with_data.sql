45200512826902df7c0c881bf4ec2ab68ef19104
Sitthidet-SR <Sitthidet.SR@gmail.com>
2025-11-20 18:33:39 +0700

    first commit

--- sql/devshop_with_data.sql
+++ sql/devshop_with_data.sql
@@ -583,7 +583,7 @@ CREATE TABLE `wishlist` (
   KEY `idx_user` (`user_id`),
   CONSTRAINT `wishlist_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`) ON DELETE CASCADE,
   CONSTRAINT `wishlist_ibfk_2` FOREIGN KEY (`course_id`) REFERENCES `courses` (`course_id`) ON DELETE CASCADE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -604,4 +604,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2025-11-19 21:33:21
+-- Dump completed on 2025-11-20 18:32:55
